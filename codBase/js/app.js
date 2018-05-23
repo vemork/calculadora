2a641e7fc35f6d64c681fb4bbb75a87a3bad877f
derubio <derubio1@gmail.com>
2018-05-22 23:47:18 -0500

    operaciones basicas ok

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -9,16 +9,73 @@ function aumentar(event){
 	event.style.padding = "0px";
 }
 
+function calcular(opera){
+	display.innerHTML  = eval((opera));
+	//console.log(eval((opera)));
+}
+
 function pantalla(e){
 	var display = document.getElementById("display");
+	var flag = true;
 
 	if ( e.id == "on" ){
 		display.innerHTML  = "0";
 	}else{
 		if ( display.innerHTML == 0 ){
-			display.innerHTML = e.id;
+
+			switch(e.id) {
+			    case "menos":
+			        display.innerHTML = "-";
+			    break;
+			    case "mas":
+			        display.innerHTML = "+";
+			    break;
+			    case "por":
+			        display.innerHTML = "*";
+			    break;
+			    case "dividido":
+			        display.innerHTML = "/";
+			    break;
+			    case "punto":
+			     	display.innerHTML = display.innerHTML + ".";
+			     	flag =  false;
+			    break;
+			    default:
+			        display.innerHTML = e.id;
+			}
+			
 		}else{
-			display.innerHTML = display.innerHTML + e.id;
+			switch(e.id) {
+			    case "menos":
+			        display.innerHTML = display.innerHTML +  "-";
+			        flag = true;
+			    break;
+			    case "mas":
+			         display.innerHTML = display.innerHTML +  "+";
+			         flag = true;
+			    break;
+			    case "por":
+			        display.innerHTML = display.innerHTML +  "*";
+			        flag = true;
+			    break;
+			    case "dividido":
+			        display.innerHTML = display.innerHTML +  "/";
+			        flag = true;
+			    break;
+			    case "igual":
+			      this.calcular(display.innerHTML);
+			    break;
+			    case "punto":
+			    	console.log(flag);
+			    	if ( flag ){ 
+			        	display.innerHTML = display.innerHTML +  ".";
+			        	flag = false;
+			    	}
+			    break;
+			    default:
+			        display.innerHTML = display.innerHTML + e.id;
+			}
+			
 		}	
 	}
 	
